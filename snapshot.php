775a44797db442a8eca6f607685821f2551495ff
danii-nebot <daniign@gmail.com>
2013-11-16 02:35:00 +0000

    Update code, add filter and decode input stream

--- snapshot.php
+++ snapshot.php
@@ -1,9 +1,18 @@
 <?php
-
+  // read input stream
 	$data = file_get_contents("php://input");
-	$fic_name = 'snapshot'.rand(1000,9999).'.jpg';
+	
+	// filtering and decoding code adapted from
+  // http://stackoverflow.com/questions/11843115/uploading-canvas-context-as-image-using-ajax-and-php?lq=1
+	// Filter out the headers (data:,) part.
+	$filteredData=substr($data, strpos($data, ",")+1);
+	// Need to decode before saving since the data we received is already base64 encoded
+	$decodedData=base64_decode($filteredData);
+
+	// store in server
+	$fic_name = 'snapshot'.rand(1000,9999).'.png';
 	$fp = fopen('./snapshots/'.$fic_name, 'wb');
-	$ok = fwrite( $fp, $data);
+	$ok = fwrite( $fp, $decodedData);
 	fclose( $fp );
 	if($ok)
 		echo $fic_name;
